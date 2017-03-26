1f73c48f8603ee0e512d9359c8d6ef1fe3316e77
Eugene Poddubnyy <gvedop@gmail.com>
2017-03-26 21:56:21 +0500

    Platform Controller Created.

---
+++ Assets/Scripts/PlatrformController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace TennisGame.Assets.Scripts
+{
+    public class PlatrformController: MonoBehaviour
+    {
+        public float speed = 600f;
+        public float xMin = -400f;
+        public float xMax = 400f;
+        private Rigidbody2D _rigidbody;
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            var movement = new Vector2(Input.GetAxis("Horizontal"), 0.0f);
+            _rigidbody.velocity = movement * speed;
+            _rigidbody.position = new Vector2(Mathf.Clamp(_rigidbody.position.x, xMin, xMax), 0.0f);
+            
+        }
+
+        private void Update()
+        {
+        }
+    }
+}
