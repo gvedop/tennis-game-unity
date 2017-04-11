cc57279a4231c5475dce807ebffe0aad4c0e1220
Eugene Poddubnyy <gvedop@gmail.com>
2017-04-11 15:45:29 +0500

    Ball created.

---
+++ Assets/Scripts/Actors/Ball.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace TennisGame.Assets.Scripts.Actors
+{
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(SpriteRenderer), typeof(CircleCollider2D), typeof(Rigidbody2D))]
+    public class Ball: MonoBehaviour
+    {
+        public float Speed = 300f;
+
+        private SpriteRenderer selfSpriteRenderer;
+        private CircleCollider2D selfCollider;
+        private Rigidbody2D selfRigidbody;
+
+        private void Awake()
+        {
+            selfSpriteRenderer = GetComponent<SpriteRenderer>();
+            if (selfSpriteRenderer == null)
+                throw new UnassignedReferenceException("SpriteRenderer doesn't set.");
+            selfCollider = GetComponent<CircleCollider2D>();
+            if (selfCollider == null)
+                throw new UnassignedReferenceException("CircleCollider2D doesn't set.");
+            selfRigidbody = GetComponent<Rigidbody2D>();
+            if (selfRigidbody == null)
+                throw new UnassignedReferenceException("Rigidbody2D doesn't set.");
+        }
+
+        // 1  -0.5  0  0.5   1  <- x value
+        private float hitFactor(float ballPos, float platformPos, float platformWidth)
+        {
+            return (ballPos - platformPos) / platformWidth;
+        }
+    }
+}
