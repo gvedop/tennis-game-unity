fad88a4af39fea541a5b9e7ee5b7d199e62b9601
Eugene Poddubnyy <gvedop@gmail.com>
2017-04-12 19:00:35 +0500

    VectorExtension created.

---
+++ Assets/Scripts/Core/VectorExtension.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace TennisGame.Core.Extensions
+{
+    public static class VectorExtension
+    {
+        public static Vector2 WithAddX(this Vector2 obj, float value)
+        {
+            return new Vector2(obj.x + value, obj.y);
+        }
+
+        public static Vector2 WithSubX(this Vector2 obj, float value)
+        {
+            return new Vector2(obj.x - value, obj.y);
+        }
+
+        public static Vector2 WithAddY(this Vector2 obj, float value)
+        {
+            return new Vector2(obj.x, obj.y + value);
+        }
+
+        public static Vector2 WithSubY(this Vector2 obj, float value)
+        {
+            return new Vector2(obj.x, obj.y - value);
+        }
+    }
+}
