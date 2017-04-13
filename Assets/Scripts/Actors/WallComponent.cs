38502b6eb48e684a9a813b1c4393b40faec3e0a0
Eugene Poddubnyy <gvedop@gmail.com>
2017-04-13 19:50:18 +0500

    WallComponent changed.

--- Assets/Scripts/Actors/WallComponent.cs
+++ Assets/Scripts/Actors/WallComponent.cs
@@ -8,10 +8,27 @@ namespace TennisGame.Actors
     {
         private BoxCollider2D selfCollider;
 
+        public void Init(Vector2 size, Vector2 position)
+        {
+            selfCollider.size = size;
+            transform.localPosition = position;
+        }
+
+        public void IgnoreCollision(Collider2D collider)
+        {
+            Physics2D.IgnoreCollision(selfCollider, collider);
+        }
+
+        public void IgnoreCollisions(params Collider2D[] colliders)
+        {
+            foreach (var col in colliders)
+                Physics2D.IgnoreCollision(selfCollider, col);
+        }
+
         private void Awake()
         {
             selfCollider = GetComponent<BoxCollider2D>();
-            if (selfCollider == null)
+            if (!selfCollider)
                 throw new UnassignedReferenceException("BoxCollider2D doesn't set.");
         }
     }
--- Assets/Scripts/Game/MainSceneControllerComponent.cs
+++ Assets/Scripts/Game/MainSceneControllerComponent.cs
@@ -16,13 +16,28 @@ namespace TennisGame.Game
         public Canvas FieldCanvas;
         public RectTransform FieldRect;
         public Camera FieldCamera;
+        public float WallWidth = 100f;
 
         private Field field;
 
         private void Awake()
         {
-            if (!FieldCanvas)
-                throw new UnassignedReferenceException("FieldCanvas doesn't set.");
+            if (!TopPlatform)
+                throw new UnassignedReferenceException("TopPlatform doesn't set.");
+            if (!BottomPlatform)
+                throw new UnassignedReferenceException("BottomPlatform doesn't set.");
+            if (!TopWall)
+                throw new UnassignedReferenceException("TopWall doesn't set.");
+            if (!BottomWall)
+                throw new UnassignedReferenceException("BottomWall doesn't set.");
+            if (!LeftWall)
+                throw new UnassignedReferenceException("LeftWall doesn't set.");
+            if (!RightWall)
+                throw new UnassignedReferenceException("RightWall doesn't set.");
+            if (!RightWall)
+                throw new UnassignedReferenceException("RightWall doesn't set.");
+            if (!Ball)
+                throw new UnassignedReferenceException("Ball doesn't set.");
             if (!FieldRect)
                 throw new UnassignedReferenceException("FieldRect doesn't set.");
             if (!FieldCamera)
@@ -48,27 +63,16 @@ namespace TennisGame.Game
             BottomPlatform.SetLocalPosition(field.BottomCenter.WithAddY(BottomPlatform.SelfSpriteRenderer.bounds.size.y / 2f));
             BottomPlatform.SetBorder(field.BottomLeftCorner.x, field.BottomRightCorner.x);
 
+            var middleWallWidth = WallWidth / 2f;
+            TopWall.Init(new Vector2(field.Width + 2 * WallWidth, WallWidth), field.TopCenter.WithAddY(middleWallWidth));
+            BottomWall.Init(new Vector2(field.Width + 2 * WallWidth, WallWidth), field.BottomCenter.WithSubY(middleWallWidth));
+            LeftWall.Init(new Vector2(WallWidth, field.Height), field.LeftCenter.WithSubX(middleWallWidth));
+            RightWall.Init(new Vector2(WallWidth, field.Height), field.RightCenter.WithAddX(middleWallWidth));
 
-            //player.transform.localScale = player.transform.localScale * field.WidthScale;
-            //adversary.transform.localScale = adversary.transform.localScale * field.WidthScale;
-            //ball.transform.localScale = ball.transform.localScale * field.WidthScale;
-
-            //var playerMiddleSizeY = player.GetComponent<SpriteRenderer>().bounds.size.y / 2f;
-            //player.transform.localPosition = field.BottomCenter.WithAddY(playerMiddleSizeY);
-            //player.xMin = field.BottomLeftCorner.x;
-            //player.xMax = field.BottomRightCorner.x;
-            //player.yPosition = field.BottomCenter.y;
-            ////player.speed = player.speed * field.HeightScale;
-
-            //var adversaryMiddleSizeY = adversary.GetComponent<SpriteRenderer>().bounds.size.y / 2f;
-            //adversary.transform.localPosition = field.TopCenter.WithSubY(adversaryMiddleSizeY);
-            //adversary.xMin = field.TopLeftCorner.x;
-            //adversary.xMax = field.TopRightCorner.x;
-            //adversary.yPosition = field.TopCenter.y;
-            ////adversary.speed = adversary.speed * field.HeightScale;
-
-            //ball.transform.localPosition = field.Center;
-            //ball.scale = field.WidthScale;
+            TopWall.IgnoreCollisions(TopPlatform.SelfCollider, BottomPlatform.SelfCollider);
+            BottomWall.IgnoreCollisions(TopPlatform.SelfCollider, BottomPlatform.SelfCollider);
+            LeftWall.IgnoreCollisions(TopPlatform.SelfCollider, BottomPlatform.SelfCollider);
+            RightWall.IgnoreCollisions(TopPlatform.SelfCollider, BottomPlatform.SelfCollider);
         }
     }
 }
