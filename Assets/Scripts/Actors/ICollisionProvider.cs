729fd174ab547802be2d952a916d39455155d37b
Eugene Poddubnyy <gvedop@gmail.com>
2017-04-14 11:52:08 +0500

    ICollisionProvider created.

---
+++ Assets/Scripts/Actors/ICollisionProvider.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace TennisGame.Actors
+{
+    public interface ICollisionProvider
+    {
+        float GetCollisionHitFactor(Vector2 position);
+        Vector2 GetCollisionDirection();
+        float GetCollisionAdditionalForce();
+    }
+}
