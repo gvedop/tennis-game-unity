d39a97be743c72a5c4850230ab175f02659a5139
Eugene Poddubnyy <gvedop@gmail.com>
2017-04-14 19:47:24 +0500

    Register and unregister GameController.

--- Assets/Scripts/Game/MainSceneControllerComponent.cs
+++ Assets/Scripts/Game/MainSceneControllerComponent.cs
@@ -1,26 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using TennisGame.Actors;
 using TennisGame.Core;
-using System.Collections;
 
 namespace TennisGame.Game
 {
-
-
     public class MainSceneControllerComponent : MonoBehaviour, IGameController
     {
         [SerializeField]
         private ActorSet actors;
-
-        public Canvas FieldCanvas;
-        public RectTransform FieldRect;
-        public Camera FieldCamera;
-        public float WallWidth = 100f;
-        public float PlatformSpeed = 300f;
-        public float BallSpeed = 300f;
-
+        [SerializeField]
+        private Canvas fieldCanvas;
+        [SerializeField]
+        private RectTransform fieldRect;
+        [SerializeField]
+        private Camera fieldCamera;
+        [SerializeField]
+        private float wallWidth = 100f;
+        [SerializeField]
+        private float platformSpeed = 300f;
+        [SerializeField]
+        private float ballSpeed = 300f;
         private Field field;
 
         private void Awake()
@@ -34,49 +33,49 @@ namespace TennisGame.Game
 
         private void OnDestroy()
         {
+            foreach (var actor in actors)
+                actor.UnregisterGameController();
         }
 
         private void InitComponents()
         {
-            field = new Field(FieldCanvas, FieldRect, FieldCamera);
+            field = new Field(fieldCanvas, fieldRect, fieldCamera);
 
             actors.TopPlatform.MulLocalScale(field.WidthScale);
             actors.TopPlatform.SetLocalPosition(field.TopCenter.WithSubY(actors.TopPlatform.MiddleHeight));
             actors.TopPlatform.SetBorder(field.TopLeftCorner.x, field.TopRightCorner.x);
-            actors.TopPlatform.Speed = PlatformSpeed;
-            actors.TopPlatform.RegisterGameController(this);
+            actors.TopPlatform.Speed = platformSpeed;
 
             actors.BottomPlatform.MulLocalScale(field.WidthScale);
             actors.BottomPlatform.SetLocalPosition(field.BottomCenter.WithAddY(actors.BottomPlatform.MiddleHeight));
             actors.BottomPlatform.SetBorder(field.BottomLeftCorner.x, field.BottomRightCorner.x);
-            actors.BottomPlatform.Speed = PlatformSpeed;
-            actors.BottomPlatform.RegisterGameController(this);
+            actors.BottomPlatform.Speed = platformSpeed;
 
             actors.Ball.MulLocalScale(field.WidthScale);
             actors.Ball.SetLocalPosition(field.Center);
-            actors.Ball.Speed = BallSpeed;
+            actors.Ball.Speed = ballSpeed;
 
-            var middleWallWidth = WallWidth / 2f;
-            var doubleWallWidth = field.Width + 2 * WallWidth;
+            var middleWallWidth = wallWidth / 2f;
+            var doubleWallWidth = field.Width + 2 * wallWidth;
 
-            actors.WallSet.TopWall.SetSize(new Vector2(doubleWallWidth, WallWidth));
+            actors.WallSet.TopWall.SetSize(new Vector2(doubleWallWidth, wallWidth));
             actors.WallSet.TopWall.SetLocalPosition(field.TopCenter.WithAddY(middleWallWidth));
             actors.WallSet.TopWall.IgnoreCollisions(actors.TopPlatform.SelfCollider, actors.BottomPlatform.SelfCollider);
 
-            actors.WallSet.BottomWall.SetSize(new Vector2(doubleWallWidth, WallWidth));
+            actors.WallSet.BottomWall.SetSize(new Vector2(doubleWallWidth, wallWidth));
             actors.WallSet.BottomWall.SetLocalPosition(field.BottomCenter.WithSubY(middleWallWidth));
             actors.WallSet.BottomWall.IgnoreCollisions(actors.TopPlatform.SelfCollider, actors.BottomPlatform.SelfCollider);
 
-            actors.WallSet.LeftWall.SetSize(new Vector2(WallWidth, field.Height));
+            actors.WallSet.LeftWall.SetSize(new Vector2(wallWidth, field.Height));
             actors.WallSet.LeftWall.SetLocalPosition(field.LeftCenter.WithSubX(middleWallWidth));
             actors.WallSet.LeftWall.IgnoreCollisions(actors.TopPlatform.SelfCollider, actors.BottomPlatform.SelfCollider);
 
-            actors.WallSet.RightWall.SetSize(new Vector2(WallWidth, field.Height));
+            actors.WallSet.RightWall.SetSize(new Vector2(wallWidth, field.Height));
             actors.WallSet.RightWall.SetLocalPosition(field.RightCenter.WithAddX(middleWallWidth));
             actors.WallSet.RightWall.IgnoreCollisions(actors.TopPlatform.SelfCollider, actors.BottomPlatform.SelfCollider);
-
+            
             foreach (var actor in actors)
-                Debug.Log(actor.ActorName);
+                actor.RegisterGameController(this);
         }
     }
 }
